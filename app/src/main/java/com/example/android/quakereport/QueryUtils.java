6334dce14691eec5ace9cee033dda458c9ee0d50
Sagar <sagar.0dev@gmail.com>
2021-09-01 00:14:36 +0530

    initial commit

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -21,18 +21,11 @@ import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Helper methods related to requesting and receiving earthquake data from USGS.
- */
 public final class QueryUtils {
 
 
     public static final String LOG_TAG = QueryUtils.class.getSimpleName();
-/**
-     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
-     * This class is only meant to hold static variables and methods, which can be accessed
-     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
-     */
+
     private QueryUtils() {
     }
 
